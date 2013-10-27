d6b4fbd2b4fd81e2ba46973eb3721e1a236d8888
Stefan Armbruster <stefan@armbruster-it.de>
2013-10-27 21:40:56 +0100

    get rid of TypedInjectable (has been removed in Neo4j 2.0)

--- src/main/java/org/neo4j/extension/querykiller/Lifecycle.java
+++ src/main/java/org/neo4j/extension/querykiller/Lifecycle.java
@@ -7,11 +7,10 @@ import org.neo4j.server.AbstractNeoServer;
 import org.neo4j.server.NeoServer;
 import org.neo4j.server.plugins.Injectable;
 import org.neo4j.server.plugins.SPIPluginLifecycle;
-import org.neo4j.server.plugins.TypedInjectable;
 
 import java.util.Collection;
 
-import static java.util.Collections.*;
+import static java.util.Collections.singletonList;
 
 public class Lifecycle implements SPIPluginLifecycle {
     @Override
@@ -19,13 +18,23 @@ public class Lifecycle implements SPIPluginLifecycle {
 
 
         Guard guard = neoServer.getDatabase().getGraph().getDependencyResolver().resolveDependency(Guard.class);
-        QueryRegistry queryRegistry = new QueryRegistry(guard);
+        final QueryRegistry queryRegistry = new QueryRegistry(guard);
 
         AbstractNeoServer abstractNeoServer = (AbstractNeoServer)neoServer;
         abstractNeoServer.getWebServer().addFilter(new QueryKillerFilter(queryRegistry), "/cypher"); // "/*" for catch all
 
+        Collection result = singletonList(new Injectable<QueryRegistry>() {
 
-        Collection result = singletonList(TypedInjectable.injectable(queryRegistry));
+            @Override
+            public QueryRegistry getValue() {
+                return queryRegistry;
+            }
+
+            @Override
+            public Class<QueryRegistry> getType() {
+                return QueryRegistry.class;
+            }
+        });
         return result;
     }
 
