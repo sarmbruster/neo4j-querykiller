8298bee9436382fb0789c0dd7aa073142f0a2834
Stefan Armbruster <stefan@armbruster-it.de>
2015-04-04 14:17:03 +0200

    use jersey-media-json-jackson for rendering

--- src/main/java/org/neo4j/extension/querykiller/statistics/StatisticsService.java
+++ src/main/java/org/neo4j/extension/querykiller/statistics/StatisticsService.java
@@ -1,14 +1,10 @@
 package org.neo4j.extension.querykiller.statistics;
 
-import org.codehaus.jackson.map.ObjectMapper;
-import org.neo4j.extension.querykiller.QueryRegistryEntry;
-import org.neo4j.extension.querykiller.QueryRegistryExtension;
-
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.util.Map;
 
 @Path("/")
 public class StatisticsService {
@@ -27,24 +23,10 @@ public class StatisticsService {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response statistics() throws IOException {
+    public Map<String, QueryStat> statistics() throws IOException {
+        return queryStatisticsExtension.getSortedStatistics();
 
-/*        long now = System.currentTimeMillis();
-        List<Map<String,Object>> result = new ArrayList<>();
-        for (QueryRegistryEntry q : queryStatisticsExtension.getRunningQueries()) {
-            Map<String, Object> map = new HashMap<>();
-            map.put("cypher", q.getCypher());
-            map.put("key", q.getKey());
-            map.put("since", now - q.getStarted().getTime());
-            map.put("thread", q.getThread());
-            map.put("remoteHost", q.getRemoteHost());
-            map.put("remoteUser", q.getRemoteUser());
-            map.put("endPoint", q.getEndPoint());
-            result.add(map);
-        }*/
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        return Response.ok().entity(objectMapper.writeValueAsString(queryStatisticsExtension.getSortedStatistics())).build();
     }
 
 /*
