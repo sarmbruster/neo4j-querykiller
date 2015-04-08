88b43c40de8e68852b3d71586ccde592a90a83dc
Stefan Armbruster <stefan@armbruster-it.de>
2015-04-08 17:13:18 +0200

    refactoring

--- src/main/java/org/neo4j/extension/querykiller/filter/LegacyCypherQueryKillerFilter.java
+++ src/main/java/org/neo4j/extension/querykiller/filter/LegacyCypherQueryKillerFilter.java
@@ -1,26 +1,78 @@
 package org.neo4j.extension.querykiller.filter;
 
 import java.io.IOException;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 
+import org.codehaus.jackson.map.ObjectMapper;
+import org.neo4j.extension.querykiller.QueryRegistryEntry;
 import org.neo4j.extension.querykiller.QueryRegistryExtension;
+import org.neo4j.extension.querykiller.http.CopyHttpServletRequest;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * parses the cypher statement from a call to the legacy cypher endpoint
  */
-public class LegacyCypherQueryKillerFilter extends QueryKillerFilter {
+public class LegacyCypherQueryKillerFilter implements Filter {
 
-    public LegacyCypherQueryKillerFilter(QueryRegistryExtension queryRegistryExtension,
-                                         GraphDatabaseService graphDatabaseService) {
-        super(queryRegistryExtension, graphDatabaseService);
+    public final Logger log = LoggerFactory.getLogger(LegacyCypherQueryKillerFilter.class);
+
+    final protected QueryRegistryExtension queryRegistryExtension;
+    final protected GraphDatabaseService graphDatabaseService;
+
+    private ObjectMapper objectMapper;
+
+    public LegacyCypherQueryKillerFilter(QueryRegistryExtension queryRegistryExtension, GraphDatabaseService graphDatabaseService) {
+        this.queryRegistryExtension = queryRegistryExtension;
+        this.graphDatabaseService = graphDatabaseService;
+    }
+
+    public ObjectMapper getObjectMapper() {
+        return objectMapper;
     }
 
     @Override
-    protected String extractCypherFromRequest(HttpServletRequest copyRequest)
-    {
+    public void init(FilterConfig filterConfig) throws ServletException {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        String requestURI = ((HttpServletRequest) request).getRequestURI();
+        log.debug("intercepting request " + requestURI);
+        HttpServletRequest copyRequest = new CopyHttpServletRequest((HttpServletRequest) request);
+
+        QueryRegistryEntry queryMapEntry = null;
+        String cypher = extractCypherFromRequest(copyRequest);
+        Transaction tx = graphDatabaseService.beginTx();
         try {
-            return getObjectMapper().readTree( copyRequest.getReader() ).get( "query" ).getTextValue();
+            queryMapEntry = queryRegistryExtension.registerQuery(
+                    tx,
+                    cypher,
+                    copyRequest.getPathInfo(),
+                    copyRequest.getRemoteHost(),
+                    copyRequest.getRemoteUser());
+            chain.doFilter(copyRequest, response);
+            tx.success();
+        } finally {
+            queryRegistryExtension.unregisterQuery(queryMapEntry);
+            tx.close();
+            log.debug("intercepting request DONE");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        // intentionally empty
+    }
+
+    protected String extractCypherFromRequest(HttpServletRequest copyRequest) {
+        try {
+            return getObjectMapper().readTree(copyRequest.getReader()).get("query").getTextValue();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/org/neo4j/extension/querykiller/filter/TransactionalCypherQueryKillerFilter.java
+++ src/main/java/org/neo4j/extension/querykiller/filter/TransactionalCypherQueryKillerFilter.java
@@ -2,8 +2,10 @@ package org.neo4j.extension.querykiller.filter;
 
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.neo4j.extension.querykiller.QueryRegistryEntry;
 import org.neo4j.extension.querykiller.QueryRegistryExtension;
+import org.neo4j.extension.querykiller.http.CopyHttpServletRequest;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.server.rest.transactional.ExecutionResultSerializer;
@@ -16,7 +18,7 @@ import org.neo4j.server.rest.web.TransactionUriScheme;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.WebApplicationException;
@@ -38,7 +40,7 @@ import static org.neo4j.extension.querykiller.filter.RequestType.*;
 /**
  * parse cypher from request when transactional endpoint is used
  */
-public class TransactionalCypherQueryKillerFilter extends QueryKillerFilter {
+public class TransactionalCypherQueryKillerFilter implements Filter {
 
     public static final String URI_ONE_SHOT = "/db/data/transaction/commit";
     public static final Pattern URI_BEGIN = Pattern.compile("^/db/data/transaction/?");
@@ -46,17 +48,29 @@ public class TransactionalCypherQueryKillerFilter extends QueryKillerFilter {
     public static final Pattern URI_COMMIT = Pattern.compile("^/db/data/transaction/(\\d+)/commit/?$");
     public final Logger log = LoggerFactory.getLogger(TransactionalCypherQueryKillerFilter.class);
 
-
-    protected TransactionRegistry transactionRegistry;
-    protected TransactionFacade transactionFacade;
-
+    final protected QueryRegistryExtension queryRegistryExtension;
+    final protected GraphDatabaseService graphDatabaseService;
+    final protected TransactionRegistry transactionRegistry;
+    final protected TransactionFacade transactionFacade;
     protected Field transactionHandleIdField;
+
+    private ObjectMapper objectMapper;
 
     public TransactionalCypherQueryKillerFilter(QueryRegistryExtension queryRegistryExtension,
                                                 GraphDatabaseService graphDatabaseService, TransactionRegistry transactionRegistry, TransactionFacade transactionFacade) {
-        super(queryRegistryExtension, graphDatabaseService);
+        this.queryRegistryExtension = queryRegistryExtension;
+        this.graphDatabaseService = graphDatabaseService;
         this.transactionRegistry = transactionRegistry;
         this.transactionFacade = transactionFacade;
+    }
+
+    public ObjectMapper getObjectMapper() {
+        return objectMapper;
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        objectMapper = new ObjectMapper();
         try {
             transactionHandleIdField = TransactionHandle.class.getDeclaredField("id");
             transactionHandleIdField.setAccessible(true);
@@ -66,6 +80,42 @@ public class TransactionalCypherQueryKillerFilter extends QueryKillerFilter {
     }
 
     @Override
+    public void destroy() {
+        // intentionally empty
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        String requestURI = ((HttpServletRequest) request).getRequestURI();
+        log.debug("intercepting request " + requestURI);
+        HttpServletRequest copyRequest = new CopyHttpServletRequest((HttpServletRequest) request);
+
+        RequestType requestType = determineRequestType(copyRequest);
+
+        QueryRegistryEntry queryMapEntry = null;
+        String cypher = extractCypherFromRequest(copyRequest);
+        Transaction tx = getTransaction(requestType, copyRequest);
+        try {
+            queryMapEntry = preProcess(requestType, copyRequest, cypher, tx, response);
+
+            if (!requestType.equals(BEGIN)) {
+                chain.doFilter(copyRequest, response);
+            }
+            if (tx != null) {
+                tx.success();
+            }
+//            } catch (Exception e) {
+//                throw e;
+        } finally {
+            postProcess(requestType, copyRequest, cypher, tx, queryMapEntry, (HttpServletResponse) response);
+            if (tx != null) {
+                tx.close();
+            }
+            log.debug("intercepting request DONE");
+        }
+    }
+
     protected String extractCypherFromRequest(HttpServletRequest copyRequest) {
         try {
             JsonNode tree = getObjectMapper().readTree(copyRequest.getReader());
@@ -81,7 +131,6 @@ public class TransactionalCypherQueryKillerFilter extends QueryKillerFilter {
         }
     }
 
-    @Override
     protected RequestType determineRequestType(HttpServletRequest copyRequest) {
         String requestURI = copyRequest.getRequestURI();
         if (requestURI.equals(URI_ONE_SHOT)) {
@@ -97,52 +146,26 @@ public class TransactionalCypherQueryKillerFilter extends QueryKillerFilter {
         }
     }
 
-    @Override
     protected Transaction getTransaction(RequestType requestType, HttpServletRequest request) {
-        switch (requestType) {
-            case ONE_SHOT:
-                return super.getTransaction(requestType, request);
-
-            case BEGIN:
-                return null;
-                //return super.getTransaction(requestType, request);
-
-            case AMEND:
-                return null;
-
-            case COMMIT:
-                return null;
-
-            case ROLLBACK:
-                return null;
-
-            default:
-                throw new IllegalStateException("SHOULD NOT HAPPEN");
-        }
+        return requestType == ONE_SHOT ? graphDatabaseService.beginTx() : null;
     }
 
-    @Override
     protected QueryRegistryEntry preProcess(RequestType requestType, HttpServletRequest request, String cypher, Transaction tx, ServletResponse response) {
         long txId;
-        Matcher matcher;
         switch (requestType) {
             case ONE_SHOT:
-                return super.preProcess(requestType, request, cypher, tx, response);
+                return queryRegistryExtension.registerQuery(tx, cypher, request.getPathInfo(), request.getRemoteHost(), request.getRemoteUser());
 
             case BEGIN:
                 return null;
 
             case AMEND:
-                matcher = URI_AMEND.matcher(request.getRequestURI());
-                matcher.matches();
-                txId = Long.parseLong(matcher.group(1));
+                txId = getTxIdFromRegex(URI_AMEND, request);
                 return queryRegistryExtension.registerQuery(new TransactionalEndpointTransactionWrapper(transactionRegistry, txId),
                         cypher, request.getRequestURI(), request.getRemoteHost(), request.getRemoteUser());
 
             case COMMIT:
-                matcher = URI_COMMIT.matcher(request.getRequestURI());
-                matcher.matches();
-                txId = Long.parseLong(matcher.group(1));
+                txId = getTxIdFromRegex(URI_COMMIT, request);
                 return queryRegistryExtension.registerQuery(new TransactionalEndpointTransactionWrapper(transactionRegistry, txId),
                         cypher, request.getRequestURI(), request.getRemoteHost(), request.getRemoteUser());
 
@@ -154,56 +177,29 @@ public class TransactionalCypherQueryKillerFilter extends QueryKillerFilter {
         }
     }
 
-    @Override
-    protected boolean shouldRunFilterChain(RequestType requestType) {
-        return !requestType.equals(BEGIN);
+    protected long getTxIdFromRegex(Pattern pattern, HttpServletRequest request ) {
+        Matcher matcher = pattern.matcher(request.getRequestURI());
+        matcher.matches();
+        return Long.parseLong(matcher.group(1));
     }
 
-    @Override
     protected void postProcess(RequestType requestType, final HttpServletRequest request, String cypher, Transaction tx, QueryRegistryEntry queryMapEntry, HttpServletResponse response) {
         switch (requestType) {
             case ONE_SHOT:
-                super.postProcess(requestType, request, cypher, tx, queryMapEntry, response);
+                queryRegistryExtension.unregisterQuery(queryMapEntry);
                 break;
 
             case BEGIN:
 
-                try {
-                    final URI baseUri = new URI(request.getRequestURL().toString());
-
-                    // here were basically copying the code from TransactionService#executeStatementsInNewTransaction
-                    // and register that one with our QueryRegistry
-                    QueryRegistryEntry queryRegistryEntry = null;
-                    try {
-                        TransactionHandle transactionHandle = transactionFacade.newTransactionHandle(new UriBuilder(request));
-
-                        queryRegistryEntry = queryRegistryExtension.registerQuery(new TransactionalEndpointTransactionWrapper(transactionRegistry, (Long) transactionHandleIdField.get(transactionHandle)), cypher, request.getRequestURI(), request.getRemoteHost(), request.getRemoteUser());
-
-                        StreamingOutput streamingResults = executeStatements(request.getInputStream(), transactionHandle, baseUri, request);
-                        streamingResults.write(response.getOutputStream());
-                        response.setStatus(201);
-                        response.setHeader("Location", transactionHandle.uri().toString());
-
-                    } catch (TransactionLifecycleException e) {
-                        StreamingOutput streamingResults = serializeError(e.toNeo4jError(), baseUri);
-                        streamingResults.write(response.getOutputStream());
-                        response.setStatus(404);
-                    } finally {
-                        if (queryRegistryEntry!=null) {
-                            queryRegistryExtension.unregisterQuery(queryRegistryEntry);
-                        }
-                    }
-                } catch (Exception e) {  // get rid of damn checked exceptions
-                    throw new RuntimeException(e);
-                }
+                processBeginRequest(request, response, cypher);
                 break;
 
             case AMEND:
-                super.postProcess(requestType, request, cypher, tx, queryMapEntry, response);
+                queryRegistryExtension.unregisterQuery(queryMapEntry);
                 break;
 
             case COMMIT:
-                super.postProcess(requestType, request, cypher, tx, queryMapEntry, response);
+                queryRegistryExtension.unregisterQuery(queryMapEntry);
                 break;
 
             case ROLLBACK:
@@ -214,7 +210,38 @@ public class TransactionalCypherQueryKillerFilter extends QueryKillerFilter {
         }
     }
 
-    // copyied from TransactionService
+    protected void processBeginRequest(final HttpServletRequest request, HttpServletResponse response, String cypher) {
+        try {
+            final URI baseUri = new URI(request.getRequestURL().toString());
+
+            // here were basically copying the code from TransactionService#executeStatementsInNewTransaction
+            // and register that one with our QueryRegistry
+            QueryRegistryEntry queryRegistryEntry = null;
+            try {
+                TransactionHandle transactionHandle = transactionFacade.newTransactionHandle(new UriBuilder(request));
+
+                queryRegistryEntry = queryRegistryExtension.registerQuery(new TransactionalEndpointTransactionWrapper(transactionRegistry, (Long) transactionHandleIdField.get(transactionHandle)), cypher, request.getRequestURI(), request.getRemoteHost(), request.getRemoteUser());
+
+                StreamingOutput streamingResults = executeStatements(request.getInputStream(), transactionHandle, baseUri, request);
+                streamingResults.write(response.getOutputStream());
+                response.setStatus(201);
+                response.setHeader("Location", transactionHandle.uri().toString());
+
+            } catch (TransactionLifecycleException e) {
+                StreamingOutput streamingResults = serializeError(e.toNeo4jError(), baseUri);
+                streamingResults.write(response.getOutputStream());
+                response.setStatus(404);
+            } finally {
+                if (queryRegistryEntry!=null) {
+                    queryRegistryExtension.unregisterQuery(queryRegistryEntry);
+                }
+            }
+        } catch (Exception e) {  // get rid of damn checked exceptions
+            throw new RuntimeException(e);
+        }
+    }
+
+    // copied from TransactionService
 
     private StreamingOutput serializeError( final Neo4jError neo4jError, final URI baseUri )
     {
