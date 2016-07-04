a3e030db575f863a3cec01833573b4da1793a31c
Stefan Armbruster <stefan@armbruster-it.de>
2016-07-04 18:01:03 +0200

    adding tests to detect potential incompatible API changes in future Neo4j releases.

--- src/test/groovy/org/neo4j/extension/querykiller/agent/WrapNeo4jComponentsAgentSpec.groovy
+++ src/test/groovy/org/neo4j/extension/querykiller/agent/WrapNeo4jComponentsAgentSpec.groovy
@@ -17,6 +17,48 @@ class WrapNeo4jComponentsAgentSpec extends Specification {
         AgentLoader.loadAgentClass(WrapNeo4jComponentsAgent.class.name, null);
     }
 
+    private checkMethod(String className, String methodName, String... paramClassNames) {
+        def clazz = Class.forName(className)
+        List<Class> paramClasses = paramClassNames.collect { Class.forName(it) }
+        def method = clazz.getDeclaredMethod( methodName, *paramClasses)
+        assert method
+    }
+
+    private checkConstructor(String className, String... paramClassNames) {
+        def clazz = Class.forName(className)
+        List<Class> paramClasses = paramClassNames.collect { Class.forName(it) }
+        def constructor = clazz.getDeclaredConstructor(*paramClasses)
+        assert constructor
+    }
+
+    def "verify Neo4j API is ready for instrumentation"() {
+
+        when:
+        checkMethod("org.neo4j.kernel.impl.core.ThreadToStatementContextBridge", "bindTransactionToCurrentThread",
+                "org.neo4j.kernel.api.KernelTransaction")
+        checkMethod("org.neo4j.kernel.impl.core.ThreadToStatementContextBridge", "unbindTransactionFromCurrentThread")
+        checkMethod("org.neo4j.cypher.internal.javacompat.ExecutionEngine", "executeQuery", "java.lang.String", "java.util.Map", "org.neo4j.kernel.impl.query.QuerySession")
+        checkMethod("org.neo4j.cypher.internal.javacompat.ExecutionEngine", "profileQuery", "java.lang.String", "java.util.Map", "org.neo4j.kernel.impl.query.QuerySession")
+        checkMethod("org.neo4j.bolt.v1.runtime.internal.SessionStateMachine", "run", "java.lang.String", "java.util.Map", "java.lang.Object", 'org.neo4j.bolt.v1.runtime.Session$Callback')
+        checkMethod("org.neo4j.bolt.v1.runtime.internal.SessionStateMachine", "pullAll", "java.lang.Object", 'org.neo4j.bolt.v1.runtime.Session$Callback')
+
+        checkConstructor("org.neo4j.bolt.v1.runtime.internal.SessionStateMachine",
+                "java.lang.String",
+                "org.neo4j.udc.UsageData",
+                "org.neo4j.kernel.impl.factory.GraphDatabaseFacade",
+                "org.neo4j.kernel.impl.core.ThreadToStatementContextBridge",
+                "org.neo4j.bolt.v1.runtime.spi.StatementRunner",
+                "org.neo4j.kernel.impl.logging.LogService",
+                "org.neo4j.bolt.security.auth.Authentication"
+        )
+
+        //public SessionStateMachine( String connectionDescriptor, UsageData usageData, GraphDatabaseFacade db, ThreadToStatementContextBridge txBridge,
+//                    StatementRunner engine, LogService logging, Authentication authentication )
+
+        then:
+        noExceptionThrown()
+    }
+
     def "ThreadToStatementContextBridge has an eventbus"() {
         expect:
         ThreadToStatementContextBridge.class.getDeclaredField("eventBus") != null
