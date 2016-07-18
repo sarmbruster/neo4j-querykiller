63ad065b44f970c5262b74544fda0319e62edd9e
Stefan Armbruster <stefan@armbruster-it.de>
2016-07-18 16:59:40 +0200

    travis

--- upload-report.sh
+++ upload-report.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-pwd
-find .
-tar -zcf - build/reports/tests | curl --upload-file - https://transfer.sh/report.tar.gz
+env
+cat /master/settings.gradle
+#tar -zcf - build/reports/tests | curl --upload-file - https://transfer.sh/report.tar.gz
